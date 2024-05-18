25061fa35b74974f39a38a588e7458ca22701571
Stepan <65602252+StepOne-ai@users.noreply.github.com>
2024-05-18 22:43:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
+
 const urlencodedParser = express.urlencoded({extended: false});
 
 app.get('/', (req, res) => {
