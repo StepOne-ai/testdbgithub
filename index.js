c611b4fe3c46bdb582c38c9537cf807c5f2364aa
Stepan <65602252+StepOne-ai@users.noreply.github.com>
2024-05-18 22:37:39 +0300

    commit1

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+const urlencodedParser = express.urlencoded({extended: false});
+
+app.get('/', (req, res) => {
+    return res.render('index')
+});
+
+app.post('/', urlencodedParser, (req, res) => {
+  return res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`App listening at port ${port}`)
+})
+  
+module.exports = app;
