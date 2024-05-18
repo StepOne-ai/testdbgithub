4f1ce400fb5748001baec5e9a8fce3ee3d8a122b
Stepan <65602252+StepOne-ai@users.noreply.github.com>
2024-05-18 22:55:50 +0300

    commit3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "public"));
+app.use(express.static("public"));
 
 const urlencodedParser = express.urlencoded({extended: false});
 
