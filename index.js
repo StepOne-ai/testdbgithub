9fc11a6f7b72a729c7125f222f90a99474c2e189
Stepan <65602252+StepOne-ai@users.noreply.github.com>
2024-06-01 23:23:15 +0300

    commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const { uploadProcessedData, initializeFirebaseApp } = require('./lib/firebase.js')
+
 const express = require('express')
 const app = express()
 const port = 3000
+initializeFirebaseApp();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -14,6 +17,15 @@ app.get('/', (req, res) => {
 
 app.post('/', urlencodedParser, (req, res) => {
   return res.render('index')
+})
+
+app.get('/test-upload', async (req, res) => {
+  await uploadProcessedData();
+  return res.send({status: 200});
+})
+
+app.get('/ass', async (req, res) => {
+  return res.send({status: 200});
 })
 
 app.listen(port, () => {
