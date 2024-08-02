f6c879b34e773d797b514c19f03ebfe28b4add0a
Stepan <65602252+StepOne-ai@users.noreply.github.com>
2024-08-02 18:54:16 +0300

    userdata

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { uploadProcessedData, initializeFirebaseApp, getTheData } = require('./lib/firebase.js')
-
-const express = require('express')
+const { review } = require('./lib/review.js')
+const { checkUsername } = require('./lib/validate.js')
+const express = require('express');
 const app = express()
 const port = 3000
 initializeFirebaseApp();
@@ -126,6 +127,8 @@ app.get('/bmstu', urlencodedParser, async (req, res) => {
   let usernames = []
   let comments = []
   let dates = []
+  let poses = []
+  let neges = []
   let allData = await getTheData("usersBmstu");
   let alerting = 0
 
@@ -134,14 +137,18 @@ app.get('/bmstu', urlencodedParser, async (req, res) => {
     let usernameTo = JSON.stringify(data["username"]).replaceAll('"', '')
     let commentTo = JSON.stringify(data["comment"]).replaceAll('"', '')
     let dateTo = formatDate(new Date(data["date"].seconds*1000))
+    let posTo = JSON.stringify(data["pos"]).replaceAll('"', '')
+    let negTo = JSON.stringify(data["neg"]).replaceAll('"', '')
 
     users.push(nameTo)
     usernames.push(usernameTo)
     comments.push(commentTo)
     dates.push(dateTo)
-  })
+    poses.push(posTo)
+    neges.push(negTo)
+})
 
-  return res.render('index', {result: {users, usernames, comments, dates, alerting, bmstu} })
+  return res.render('index', {result: {users, usernames, comments, dates, alerting, poses, neges, bmstu} })
 });
 
 app.post('/bmstu', urlencodedParser, async (req, res) => {
@@ -149,6 +156,8 @@ app.post('/bmstu', urlencodedParser, async (req, res) => {
   let usernames = []
   let comments = []
   let dates = []
+  let poses = []
+  let neges = []
   let allData = await getTheData("usersBmstu");
   let bmstu = "bmstu"
 
@@ -158,22 +167,34 @@ app.post('/bmstu', urlencodedParser, async (req, res) => {
       let usernameTo = JSON.stringify(data["username"]).replaceAll('"', '')
       let commentTo = JSON.stringify(data["comment"]).replaceAll('"', '')
       let dateTo = formatDate(new Date(data["date"].seconds*1000))
+      let posTo = JSON.stringify(data["pos"]).replaceAll('"', '')
+      let negTo = JSON.stringify(data["neg"]).replaceAll('"', '')
 
       users.push(nameTo)
       usernames.push(usernameTo)
       comments.push(commentTo)
       dates.push(dateTo)
+      poses.push(posTo)
+      neges.push(negTo)
     })
     let alerting = 2
-    return res.render('index', {result: {users, usernames, comments, dates, alerting, bmstu} })
+    return res.render('index', {result: {users, usernames, comments, dates, alerting, poses, neges, bmstu} })
   } else {
     const name = req.body.name;
     const username = req.body.username;
     const comment = req.body.comment;
     const date = new Date();
+    const rev = await review(comment);
+    const pos = rev.pos
+    const neg = rev.neg
+    let alerting = 1
+    //Validating
+    if (await checkUsername(username) == 'Does not exist') {
+      alerting = 3
+    }
 
     //Check if the user is already in the list 0 = okay, 1 = success, 2 = error
-    let alerting = 1
+    // 3 = username does not exist
     allData.forEach(data => {
       if(data["username"] == username){
         alerting = 2
@@ -181,41 +202,53 @@ app.post('/bmstu', urlencodedParser, async (req, res) => {
     })
 
     if (alerting == 1) {
-      uploadProcessedData(name, username, comment, date, "usersBmstu");
-      console.log("upload success")
+      uploadProcessedData(name, username, comment, date, pos, neg, "usersBmstu");
       let allData = await getTheData("usersBmstu");
       allData.forEach(data => {
         let nameTo = JSON.stringify(data["name"]).replaceAll('"', '')
         let usernameTo = JSON.stringify(data["username"]).replaceAll('"', '')
         let commentTo = JSON.stringify(data["comment"]).replaceAll('"', '')
         let dateTo = formatDate(new Date(data["date"].seconds*1000))
+        let posTo = JSON.stringify(data["pos"]).replaceAll('"', '')
+        let negTo = JSON.stringify(data["neg"]).replaceAll('"', '')
 
         users.push(nameTo)
         usernames.push(usernameTo)
         comments.push(commentTo)
         dates.push(dateTo)
+        poses.push(posTo)
+        neges.push(negTo)
       })
 
-      return res.render('index', {result: {users, usernames, comments, dates, alerting, bmstu} })
+      return res.render('index', {result: {users, usernames, comments, dates, alerting, poses, neges, bmstu} })
     } else {
       allData.forEach(data => {
         let nameTo = JSON.stringify(data["name"]).replaceAll('"', '')
         let usernameTo = JSON.stringify(data["username"]).replaceAll('"', '')
         let commentTo = JSON.stringify(data["comment"]).replaceAll('"', '')
         let dateTo = formatDate(new Date(data["date"].seconds*1000))
+        let posTo = JSON.stringify(data["pos"]).replaceAll('"', '')
+        let negTo = JSON.stringify(data["neg"]).replaceAll('"', '')
 
         users.push(nameTo)
         usernames.push(usernameTo)
         comments.push(commentTo)
         dates.push(dateTo)
+        poses.push(posTo)
+        neges.push(negTo)
       })
-      return res.render('index', {result: {users, usernames, comments, dates, alerting, bmstu} })
+      return res.render('index', {result: {users, usernames, comments, dates, alerting, poses, neges, bmstu} })
     }
   }
 })
 
+// User Data
+app.get('/user', (req, res) => {
+  return res.render('ipgetter')
+});
+
 app.listen(port, () => {
-    console.log(`App listening at port ${port}`)
+  console.log(`App listening at port ${port}`)
 })
 
 module.exports = app;
